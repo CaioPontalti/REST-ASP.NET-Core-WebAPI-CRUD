6e1b6f104dd386596c45b8e7cb02d4519b30847c
CaioPontalti <43015157+CaioPontalti@users.noreply.github.com>
2020-01-15 12:51:55 -0300

    master

---
+++ APICoreCRUD/APICore/Context/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using APICore.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICore.Context
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
